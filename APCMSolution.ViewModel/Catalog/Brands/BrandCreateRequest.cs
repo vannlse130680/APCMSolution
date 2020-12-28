af75ea15caea5935f2c7353273db3926e5581982
vannlse130680 <nguyenlamvan1999@gmail.com>
2020-12-28 16:21:39 +0700

    brand create API

---
+++ APCMSolution.ViewModel/Catalog/Brands/BrandCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APCMSolution.ViewModel.Catalog.Brands
+{
+    public class BrandCreateRequest
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
